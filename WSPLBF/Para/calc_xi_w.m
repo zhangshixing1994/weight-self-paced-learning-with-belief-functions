e708dfaf05cd149ff5dd127e0893f103d4fd26cd
zhangshixing1994 <44203965+zhangshixing1994@users.noreply.github.com>
2024-06-28 10:03:01 +0800

    Update calc_xi_w.m

--- WSPLBF/Para/calc_xi_w.m
+++ WSPLBF/Para/calc_xi_w.m
@@ -1,12 +1,12 @@
-function [xi,w] = calc_xi_w(Xtrain,ytrain,Xtest,ytest,net,AM,w)
+function [xi] = calc_xi(Xtrain,ytrain,Xtest,ytest,net,AM)
 
 
 psi=1;
 xi=[1,0.8,0.6,0.4,0.2,0];
-[bestParam] = search_para(Xtrain,ytrain,Xtest,ytest,net,AM,xi,w);
+[bestParam] = search_para(Xtrain,ytrain,Xtest,ytest,net,AM,xi);
 gama= bestParam.gama;
 psi=bestParam.psi;
-w=w.^gama;
+
 xi=xi.^psi;
 end
 
