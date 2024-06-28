5c94c6df8fb20b933d6d7459732f104c334cd32a
zhangshixing1994 <44203965+zhangshixing1994@users.noreply.github.com>
2024-06-28 10:30:29 +0800

    Update calc_xi_w.m

--- WSPLBF/Para/calc_xi_w.m
+++ WSPLBF/Para/calc_xi_w.m
@@ -2,7 +2,7 @@ function [xi,w] = calc_xi_w(Xtrain,ytrain,Xtest,ytest,net,AM,w)
 
 
 psi=1;
-xi=[1,0.8,0.6,0.4,0.2,0];
+xi=[1,0.8,0.6,0.4,0.2,0];% training rounds is set as 6 in our datasets;if you use large dataset,xi needs adjust.
 [bestParam] = search_para(Xtrain,ytrain,Xtest,ytest,net,AM,xi,w);
 gama= bestParam.gama;
 psi=bestParam.psi;
